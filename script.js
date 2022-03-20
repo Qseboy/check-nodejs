4036061ea7d9bcc98acaaeae13af2cc742067efd
Qseboy <Qseboy1998@gmail.com>
2022-03-20 11:25:19 +0200

    start

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// let number = 1;
+
+
+// console.log(-4/0);
+// console.log("string" * 9);
+
+
+// const person = "Alex";
+
+// let obj = {
+//     age: 17,
+//     name: "Ivan",
+//     inUkraine: false,
+//     money: [300, 400, 300]
+// };
+
+// console.log(obj.money[1]);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// let res = +prompt("male or female?", "");
+// console.log(res+10);
+
+// const answers = [];
+// answers[0] = prompt("Сколько вам лет?", "");
+// answers[1] = prompt("Ваше имя?", "");
+// answers[2] = prompt("Какая ваша Фамилия", "");
+
+// document.write(answers);
+
+
+конкотинация;
+
+const category = "toys";
+const num = 1;
+console.log(`http://google.com/${category}/${num}`);
